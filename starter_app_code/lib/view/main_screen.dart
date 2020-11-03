8fc598b44a9169df9c688d6d4a49f214c6cef24e
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-11-03 14:14:21 +0200

    Created a separate folder with the starter app code

---
+++ starter_app_code/lib/view/main_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:setstate/utils/note_mode.dart';
+import 'package:setstate/view/pages/news_page.dart';
+import 'package:setstate/view/pages/note_view_page.dart';
+import 'package:setstate/view/pages/notes_page.dart';
+import 'package:setstate/view/pages/settings_page.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    NotesPage(),
+    NewsPage(),
+    SettingsPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.note),
+              title: Text('Notes')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.web),
+              title: Text('News')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings')
+          )
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+    );
+  }
+}
---
+++ starter_app_code/lib/view/pages/news_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:setstate/model/news.dart';
+import 'package:setstate/view/pages/news_view_page.dart';
+
+class NewsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final notes = [
+      News('News 1', 'Demo text 12321312321'),
+      News('News 2', 'Demo text 12321312321'),
+      News('News 3', 'Demo text 12321312321')
+    ];
+    return Container(
+      child: ListView.builder(
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  elevation: 1.0,
+                  margin: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, bottom: 10.0, left: 10.0, right: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        NewsTitle(notes[index].title),
+                        NewsContent(notes[index].text),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            OutlineButton(
+                              child: Text('VIEW'),
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                      return NewsViewPage(title: notes[index].title, content: notes[index].text,);
+                                    })
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+      ),
+    );
+  }
+}
+
+class NewsTitle extends StatelessWidget {
+
+  final String _title;
+  NewsTitle(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _title,
+      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class NewsContent extends StatelessWidget {
+
+  final String _text;
+  NewsContent(this._text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: Text(
+        _text,
+        maxLines: 3,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+}
