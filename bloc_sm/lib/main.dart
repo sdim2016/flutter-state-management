0455a06f274dc7e307878956dc7d39a7185ba467
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-11-08 14:14:59 +0200

    [BLoC] Settings implemented

--- bloc_sm/lib/main.dart
+++ bloc_sm/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blocsm/bloc/news_bloc/bloc.dart';
+import 'package:blocsm/bloc/settings_bloc/bloc.dart';
 import 'package:blocsm/model/repositories/news_repository.dart';
 import 'package:blocsm/model/repositories/note_repository.dart';
 import 'package:blocsm/model/repositories/settings_repository.dart';
@@ -17,16 +18,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) {
-        final newsBloc = NewsBloc(initialState: NewsLoadingState(), newsRepository: newsRepository);
-        newsBloc.add(GetNewsEvent());
-        return newsBloc;
-      },
-      child: MaterialApp(
-        title: 'Flutter State Management',
-        theme: ThemeData.light(),
-        home: MainScreen(title: 'Flutter State Management'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NewsBloc>(
+            create: (BuildContext context) {
+              final newsBloc = NewsBloc(initialState: NewsLoadingState(), newsRepository: newsRepository);
+              newsBloc.add(GetNewsEvent());
+              return newsBloc;
+            }
+        ),
+        BlocProvider<SettingsBloc>(
+            create: (BuildContext context) {
+              final settingsBloc = SettingsBloc(initialState: SettingsObtainedState(false), settingsRepository: settingsRepository);
+              settingsBloc.add(GetSettingsEvent());
+              return settingsBloc;
+            }
+        ),
+      ],
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (BuildContext context, state) {
+          ThemeData theme = (state as SettingsObtainedState).darkTheme ? ThemeData.dark() : ThemeData.light();
+          return MaterialApp(
+              title: 'Flutter State Management',
+              theme: theme,
+              home: MainScreen(title: 'Flutter State Management'),
+            );
+        },
       ),
     );
   }
--- bloc_sm/lib/view/pages/settings_page.dart
+++ bloc_sm/lib/view/pages/settings_page.dart
@@ -1,9 +1,12 @@
+import 'package:blocsm/bloc/settings_bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SettingsBloc settingsBloc = BlocProvider.of(context, listen: true);
     return Center(
       child: SettingsList(
         darkBackgroundColor: ThemeData.dark().scaffoldBackgroundColor,
@@ -15,8 +18,10 @@ class SettingsPage extends StatelessWidget {
               SettingsTile.switchTile(
                   title: 'Dark mode',
                   leading: Icon(Icons.invert_colors),
-                  onToggle: (bool value) {},
-                  switchValue: true
+                  onToggle: (bool value) {
+                    settingsBloc.add(SetSettingsEvent(value));
+                  },
+                  switchValue: (settingsBloc.state as SettingsObtainedState).darkTheme
               )
             ],
           )
